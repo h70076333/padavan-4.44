f416c9b28d19cf4c56ed4cf27e34e83c20bc44e1
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-04-24 14:01:05 +0800

    Update board.h

--- trunk/configs/boards/NEWIFI/board.h
+++ trunk/configs/boards/NEWIFI/board.h
@@ -8,7 +8,10 @@
 #define BOARD_MODEL_URL		"http://www.newifi.com/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
-
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
