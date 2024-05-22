55d7978c2f015e136cf0ac1c4b32d5bd0680fa68
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-05-22 13:10:50 +0800

    Update board.mk

--- trunk/configs/boards/WR1200JS/board.mk
+++ trunk/configs/boards/WR1200JS/board.mk
@@ -4,6 +4,6 @@
 # WR1200JS  # YOUHUA WR1200JS  # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_K2P -DBOARD_MT7615_DBDC
+CFLAGS += -DBOARD_K2P
 BOARD_NUM_USB_PORTS=2
 CONFIG_BOARD_RAM_SIZE=128
