5fa2fa8eb3f3c596da6564251f1acc49a5f774fe
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-07-07 16:18:45 +0800

    Update board.mk

--- trunk/configs/boards/MI-R3G/board.mk
+++ trunk/configs/boards/MI-R3G/board.mk
@@ -4,6 +4,6 @@
 # MI-R3P # Xiaomi R3G  # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_MIR3G
-BOARD_NUM_USB_PORTS=1
+CFLAGS += -DBOARD_CR660x -DBOARD_MT7915_DBDC
+BOARD_NUM_USB_PORTS=0
 CONFIG_BOARD_RAM_SIZE=256
