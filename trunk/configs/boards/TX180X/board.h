73b7e469d500cc2aa361153f56a4f315c982bf5b
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-07-07 15:29:23 +0800

    Update board.h

--- trunk/configs/boards/TX180X/board.h
+++ trunk/configs/boards/TX180X/board.h
@@ -1,11 +1,11 @@
-/* Xiaomi Mi R3G */
+/* TX180X */
 
-#define BOARD_PID		"MI-R3G"
-#define BOARD_NAME		"MI-R3G"
-#define BOARD_DESC		"Xiaomi Mi R3G Wireless Router"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"http://www.mi.com/"
-#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3g"
+#define BOARD_PID		"TX180X"
+#define BOARD_NAME		"TX180X"
+#define BOARD_DESC		"TX180X Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 
@@ -14,8 +14,8 @@
 #define BOARD_NUM_ANT_5G_RX	2
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
-#define BOARD_NUM_ETH_LEDS	1
-#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_NUM_UPHY_USB3	0
