3633570a71458c6e1f02d09694bc61478522c151
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-07-25 17:28:42 +0800

    Add files via upload

---
+++ trunk/configs/boards/NX18/board.h
@@ -0,0 +1,21 @@
+/* TX180X */
+
+#define BOARD_PID		"TX180X"
+#define BOARD_NAME		"TX180X"
+#define BOARD_DESC		"TX180X Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
