a2c635706ceef3104189088bd914d58f1846fff5
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-07-26 16:41:56 +0800

    Update board.mk

--- trunk/configs/boards/JCG-Y2/board.mk
+++ trunk/configs/boards/JCG-Y2/board.mk
@@ -5,5 +5,5 @@
 ##################################################################
 
 CFLAGS += -DBOARD_Y2 -DBOARD_MT7615_DBDC
-BOARD_NUM_USB_PORTS=1
+BOARD_NUM_USB_PORTS=0
 CONFIG_BOARD_RAM_SIZE=256
