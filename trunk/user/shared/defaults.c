f840164e437ca661554f8a477d1c0e4cd9ebff0c
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-08-06 15:11:13 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -767,7 +767,7 @@ struct nvram_pair router_defaults[] = {
 	
 	/*Zerotier*/
 	{ "zerotier_enable", "1" },
-	{ "zerotier_id", "0cccb752f75548e5" },
+	{ "zerotier_id", "af415e486ffa81c3" },
 	{ "zerotier_nat", "1" },
 	{ "zerotier_secret", "" },
 	{ "zero_staticnum_x", "0" },
